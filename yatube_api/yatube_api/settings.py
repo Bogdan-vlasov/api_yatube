80f0ffafb63c81a517c525f310b6a76450865744
Sprngtm4htlr <avraamchichikov@yandex.ru>
2022-06-09 20:27:14 +0300

    sprint 8

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-REST_FRAMEWORK = {        
+REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
